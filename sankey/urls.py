635a8eb6f92ccdac60e8d9905d0c737dfd1fbca2
Armin Samii <asamii@adobe.com>
2019-06-22 12:58:35 -0400

    Redo upload page

--- sankey/urls.py
+++ sankey/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('display.html', views.display, name='display'),
 ]
--- sankey/views.py
+++ sankey/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 
 from .graphCreator.sankeyCreator import SankeyConfig, makeGraphWithFile
 from .graphCreator.graphToD3 import D3Sankey
 
 def index(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
+    form = UploadFileForm()
+    return render(request, 'sankey/uploadFile.html', {'form': form})
+
+def display(request):
+    if request.method == 'POST' and request.FILES.get('sankeyJson'):
+        sankeyJson = request.FILES['sankeyJson']
 
         config = SankeyConfig();
         config.hideDecimals = request.POST.get('hideDecimals', False)
         config.hideTransferlessRounds = request.POST.get('combineWinner', False)
-        graph = makeGraphWithFile(myfile.file, config)
+        graph = makeGraphWithFile(sankeyJson.file, config)
         d3Sankey = D3Sankey(graph)
 
         return render(request, 'sankey/sankey-example.html', {
@@ -19,6 +23,6 @@ def index(request):
             'date': graph.dateString,
             'sankeyjs': d3Sankey.js
         })
+        return render(request, 'sankey/uploadFile.html', {'form': form})
     else:
-        form = UploadFileForm()
-    return render(request, 'sankey/uploadFile.html', {'form': form})
+        return redirect('index')
