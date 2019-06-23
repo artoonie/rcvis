9b4233bca195c0c38a01300e63970fca8dd31fe0
Armin Samii <asamii@adobe.com>
2019-06-23 00:23:19 -0400

    use persistent storage

--- rcvis/settings.py
+++ rcvis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sankey',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
       os.path.join(BASE_DIR, 'static/'),
 )
+
+# Uploaded media
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = None
+AWS_STORAGE_BUCKET_NAME=os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME=os.environ['AWS_S3_REGION_NAME']
+AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
