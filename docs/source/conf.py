14ad6aef51aa3aa9b236e7ea1600ca7782b9c055
Armin Samii <armin.samii@gmail.com>
2020-05-23 21:02:34 -0400

    writethedocs to readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
+import sys
+import django
+from m2r import MdInclude
+from recommonmark.transform import AutoStructify
+
+sys.path.insert(0, os.path.abspath('../../visualizer'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rcvis.settings'
+django.setup()
+
+# -- Project information -----------------------------------------------------
+
+project = 'rcvis'
+copyright = '2020, Armin Samii'
+author = 'Armin Samii'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'recommonmark'
+]
+
+# Markdown extension
+source_suffix = ['.rst', '.md']
+
+def setup(app):
+    # from m2r to make `mdinclude` work
+    app.add_config_value('no_underscore_emphasis', False, 'env')
+    app.add_config_value('m2r_parse_relative_links', False, 'env')
+    app.add_config_value('m2r_anonymous_references', False, 'env')
+    app.add_config_value('m2r_disable_inline_math', False, 'env')
+    app.add_directive('mdinclude', MdInclude)
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/source/graphCreator.rst
@@ -0,0 +1,66 @@
+Graph Loading & Creation
+===============================
+
+Colors
+-------------------------------------
+
+.. automodule:: visualizer.graphCreator.colors
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Graph
+------------------------------------
+
+.. automodule:: visualizer.graphCreator.graph
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+GraphCreator
+-------------------------------------------
+
+.. automodule:: visualizer.graphCreator.graphCreator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+GraphSummary
+-------------------------------------------
+
+.. automodule:: visualizer.graphCreator.graphSummary
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+RCV Result
+----------------------------------------
+
+.. automodule:: visualizer.graphCreator.rcvResult
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Read JSON Base
+-------------------------------------------
+
+.. automodule:: visualizer.graphCreator.readJSONBase
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Read Opavote JSON
+----------------------------------------------
+
+.. automodule:: visualizer.graphCreator.readOpaVoteJSON
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Read RCVRC JSON
+--------------------------------------------
+
+.. automodule:: visualizer.graphCreator.readRCVRCJSON
+   :members:
+   :undoc-members:
+   :show-inheritance:
