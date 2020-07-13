d8ef5792fa84553c1f0d1ecd97d5a76eea7cc9e1
Armin Samii <asamii@adobe.com>
2020-07-12 21:05:25 -0400

    Add documentation and session (web) auth

--- rcvis/settings.py
+++ rcvis/settings.py
@@ -181,6 +181,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
