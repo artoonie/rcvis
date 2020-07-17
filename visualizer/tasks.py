13a0114ef3979745ce26e29bac8837ae2794c6d7
Armin Samii <asamii@adobe.com>
2020-07-17 00:27:51 -0400

    Prep for ElasticBeanstalk deployment

--- visualizer/tasks.py
+++ visualizer/tasks.py
@@ -18,9 +18,13 @@ from visualizer.models import AutoMovie, JsonConfig
 def create_movie(pk, domain):
     """ Create a movie for the config with the given primary key, using
         a live server at the given domain """
+
     chromeOptions = webdriver.chrome.options.Options()
     chromeOptions.add_argument("--headless")
-    browser = webdriver.Chrome(options=chromeOptions)
+    chromeOptions.add_argument("--disable-dev-shm-usage")
+    chromeOptions.add_argument("--shm-size=512m")
+
+    browser = webdriver.Chrome('/usr/bin/chromedriver', options=chromeOptions)
     browser.implicitly_wait(10)
 
     jsonconfig = JsonConfig.objects.get(pk=pk)  # pylint: disable=no-member
