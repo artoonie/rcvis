fed59d4c6807a7c67392ce268134b3171cade55d
Armin Samii <armin.samii@gmail.com>
2020-08-22 14:12:41 -0400

    Intify wikipedia num votes

--- visualizer/common.py
+++ visualizer/common.py
@@ -1,4 +1,4 @@
-""" A set of common strings to be used across modules """
+""" A set of common utilities to be used across modules """
 
 INACTIVE_TEXT = "Inactive Ballots"
 RESIDUAL_SURPLUS_TEXT = "Residual Surplus"
@@ -14,3 +14,16 @@ def make_complete_url(request, urlWithoutDomain):
     scheme = 'https' if request.is_secure() else 'http'
     host = get_host(request)
     return f"{scheme}://{host}{urlWithoutDomain}"
+
+
+def intify(v):
+    """ Turn into int if it's a round number """
+    if abs(round(v) - v) < 1e-6:
+        return "%d" % round(v)
+    else:
+        return "%0.2f" % v
+
+
+def percentify(v):
+    """ Turn a float into a percentage string """
+    return str(round(1000.0 * v) / 10.0) + "%"
