007460a441d56612f2be69710dabadc361fffb6f
Armin Samii <armin.samii@gmail.com>
2020-10-26 10:17:22 -0400

    Update chrome to 86 per new stable version

--- scripts/install_chromedriver.sh
+++ scripts/install_chromedriver.sh
@@ -1,5 +1,5 @@
-# TODO - keep this in sync programmatically with .ebextensions/03_install_chrome.config
-cd /tmp # TODO - any changes must be synced to .ebextensions/03_install_chrome.config
-wget https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_linux64.zip # TODO - any changes must be synced to .ebextensions/03_install_chrome.config
-unzip chromedriver_linux64.zip # TODO - any changes must be synced to .ebextensions/03_install_chrome.config
-sudo mv chromedriver /usr/bin/chromedriver # TODO - any changes must be synced to .ebextensions/03_install_chrome.config
+# TODO - keep this in sync programmatically with .ebextensions/04_install_chrome.config
+cd /tmp # TODO - any changes must be synced to .ebextensions/04_install_chrome.config
+wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip # TODO - any changes must be synced to .ebextensions/04_install_chrome.config
+unzip chromedriver_linux64.zip # TODO - any changes must be synced to .ebextensions/04_install_chrome.config
+sudo mv chromedriver /usr/bin/chromedriver # TODO - any changes must be synced to .ebextensions/04_install_chrome.config
