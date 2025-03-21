5f00b2ab8674f8056c27f8bf84415c8d181c5413
samiik <kian.samii@autodesk.com>
2025-03-21 07:28:36 -0700

    (refactor): settings elements to dedicated page

---
+++ static/settings/settings-page-elements.js
@@ -0,0 +1,59 @@
+
+// Datatable Manipulation
+function getDatatableModal() {
+    return document.getElementById('datatable-modal');
+}
+
+function getDatatableModalContent() {
+    return document.getElementById('datatable-modal-content');
+}
+
+function getDatatableModalTitle() {
+    return document.getElementById('datatable-modal-title');
+}
+
+function getDatatableModalSubmit() {
+    return document.getElementById('datatable-modal-submit');
+}
+
+function getDatatableOuterWrapper() {
+    return document.getElementById('datatable-outer-wrapper');
+}
+
+function getDatatableUploadBox() {
+    return document.getElementById("datatable-upload-box");
+}
+
+function getDatatableOptions() {
+    return document.getElementById("data-options");
+}
+
+// Options Manipulation
+
+function getOptionsForManualSidecarEntry() {
+    return document.getElementById('manual-sidecar-options');
+}
+
+function getManuallyEditSidecarButton() {
+    return document.getElementById("manually-edit-upload-button");
+}
+
+function getManuallyEditSidecarErrorButton() {
+    return document.getElementById("manually-edit-upload-button-error");
+}
+
+function getEntireOptionsWrapper() {
+    return document.getElementById("options").style.opacity;
+}
+
+function getUploadSubmitButton() {
+    return document.getElementById("uploadButton");
+}
+
+function getEntireForm() {
+    return document.getElementById('form');
+}
+
+function getFileUploadBox() {
+    return document.getElementById("file-upload-box");
+}
