8396da63d48470fe6d160c48617f5e10c98ae43b
samiik <kian.samii@autodesk.com>
2025-03-21 18:20:29 -0700

    (fix): stricter linting.

--- eslint.config.js
+++ eslint.config.js
@@ -16,12 +16,5 @@ export default defineConfig([
 	},
 	{ files: ["**/*.js"],
         plugins: { js },
-        extends: ["js/recommended"] },
-
-	{
-		rules: {
-			"no-unused-vars": "warn",
-			"no-undef": "warn",
-		},
-	},
+        extends: ["js/recommended"] }
 ]);
